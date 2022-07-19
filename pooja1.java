90cff2f87581e0a3e7d634b2c2bc243e7d905af3
namamig <109571790+namamig@users.noreply.github.com>
2022-07-19 11:26:35 +0530

    Update pooja1.java

--- pooja1.java
+++ pooja1.java
@@ -8,6 +8,8 @@ int b=20;
 int c=a+b;
 
         System.out.println("Sum of a & b is "+c); 
+        System.out.println("Sum of a & b is "+c); 
+        System.out.println("Changes done at Github File on 19th July"); 
 
     }
 }
