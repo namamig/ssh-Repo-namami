ce51113453be83b83a090af15fb4931766ee7524
namami gupta <namamigupta711@gmail.com>
2022-07-19 10:58:13 +0530

    My first Git Hub Program

---
+++ pooja1.java
@@ -0,0 +1,15 @@
+class poo
+{
+    public static void main(String[] args)
+ {
+        System.out.println("Shri GAneshay Namah"); 
+int a=10;
+int b=20;
+int c=a+b;
+
+        System.out.println("Sum of a & b is "+c); 
+
+    }
+}
+
+
